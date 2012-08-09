d94e5a2fc6701ad7dc3e19648ad6e3eea91d2b30
Julien Bouquillon <julien@bouquillon.com>
2012-08-09 22:17:16 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+# Dynamically calculate the version based on picocms.VERSION
+version_tuple = __import__('picocms').VERSION
+version = ".".join([str(v) for v in version_tuple])
+
+setup(
+    name = 'django-picocms',
+    description = '''Simplest django CMS ever.''',
+    version = version,
+    author = 'Julien Bouquillon',
+    author_email = 'julien@revolunet.com',
+    url = 'http://github.com/revolunet/django-picocms',
+    packages=['picocms'],
+    package_data={'picocms': ['static/*']},
+    install_requires=[
+        'Django>=1.3.1,<1.5',
+        'south>=0.7.2',
+        'django-mptt>=0.5.1,<0.6'
+    ],
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
